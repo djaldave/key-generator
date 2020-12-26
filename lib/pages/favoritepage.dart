5e6cdbd095e32bff5cb8029705badd072d8153df
djaldave <djaldave@gmail.com>
2020-12-27 01:59:49 +0800

    update and delete etc

--- lib/pages/favoritepage.dart
+++ lib/pages/favoritepage.dart
@@ -4,6 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../model/passlist.dart';
 import 'package:http/http.dart' as http;
 import "dart:convert";
+import 'package:auto_size_text_field/auto_size_text_field.dart';
 
 class FavoritePage extends StatefulWidget {
   final List<PassList> passList;
@@ -16,9 +17,26 @@ class FavoritePage extends StatefulWidget {
 class _FavoritePageState extends State<FavoritePage> {
   List<PassList> _passList = [];
   bool isLoading = false;
+  int _passIndex;
+  final TextEditingController _textController = new TextEditingController();
   initState() {
     fetchPassList();
     super.initState();
+  }
+
+  int get selectedPassIndex {
+    return _passIndex;
+  }
+
+  void selectPass(int index) {
+    _passIndex = index;
+  }
+
+  PassList get selectedPassword {
+    if (selectedPassIndex == null) {
+      return null;
+    }
+    return _passList[selectedPassIndex];
   }
 
   void fetchPassList() {
@@ -39,6 +57,7 @@ class _FavoritePageState extends State<FavoritePage> {
         passwordListData.forEach((passId, passData) {
           isLoading = false;
           final PassList pstlst = PassList(id: passId, text: passData['pass']);
+
           tempList.add(pstlst);
         });
         setState(() {
@@ -46,6 +65,32 @@ class _FavoritePageState extends State<FavoritePage> {
           _passList = tempList;
         });
       }
+    });
+  }
+
+  void updatePass(int index, String pass) {
+    final Map<String, dynamic> updateData = {"pass": pass};
+
+    http
+        .put(
+            "https://pass-generator-cfa32-default-rtdb.firebaseio.com/password/${selectedPassword.id}.json",
+            body: json.encode(updateData))
+        .then((http.Response response) {
+      final PassList pstlst = PassList(id: selectedPassword.id, text: pass);
+      setState(() {
+        _passList[selectedPassIndex] = pstlst;
+      });
+    });
+  }
+
+  void deleteProduct() {
+    http
+        .delete(
+            "https://pass-generator-cfa32-default-rtdb.firebaseio.com/password/${selectedPassword.id}.json")
+        .then((http.Response response) {
+      setState(() {
+        _passList.removeAt(selectedPassIndex);
+      });
     });
   }
 
@@ -82,7 +127,7 @@ class _FavoritePageState extends State<FavoritePage> {
 
   condition(context1) {
     Widget content = Center(child: Text("No Password Added"));
-    if (_passList.length > 0 && !isLoading) {
+    if (_passList.length > 0) {
       content = Card(
           margin: EdgeInsets.all(10),
           elevation: 3,
@@ -117,13 +162,22 @@ class _FavoritePageState extends State<FavoritePage> {
                                 icon: FaIcon(FontAwesomeIcons.edit, size: 35),
                                 color: Colors.deepOrange,
                                 onPressed: () {
-                                  _startAddNewTransaction(context1);
+                                  if (_passList != null) {
+                                    selectPass(index);
+                                    print(index);
+                                    _textController.text =
+                                        _passList[index].text;
+                                    _startAddNewTransaction(context1);
+                                  }
                                 },
                               ),
                               IconButton(
                                 icon: Icon(Icons.delete, size: 40),
                                 color: Colors.red,
-                                onPressed: () {},
+                                onPressed: () {
+                                  selectPass(index);
+                                  deleteProduct();
+                                },
                               ),
                             ]),
                       ]));
@@ -160,12 +214,31 @@ class _FavoritePageState extends State<FavoritePage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: "Password"),
+                    AutoSizeTextField(
+                      style: TextStyle(fontSize: 20.0),
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        suffixIcon: CircleIconButton(onPressed: () {
+                          setState(() {
+                            _textController.clear();
+                          });
+                        }),
+                      ),
                       onSubmitted: (_) {},
                     ),
                     RaisedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        updatePass(selectedPassIndex, _textController.text);
+                        Navigator.pop(ctx);
+                        Scaffold.of(ctx).hideCurrentSnackBar();
+                        Scaffold.of(ctx).showSnackBar(SnackBar(
+                          content: Text(
+                            "UPDATED",
+                          ),
+                          duration: Duration(seconds: 2),
+                        ));
+                      },
                       child: Text("Update"),
                       color: Theme.of(context).primaryColor,
                       textColor: Theme.of(context).textTheme.button.color,
@@ -178,5 +251,35 @@ class _FavoritePageState extends State<FavoritePage> {
             onTap: () {},
           );
         });
+  }
+}
+
+class CircleIconButton extends StatelessWidget {
+  final double size;
+  final Function onPressed;
+  final IconData icon;
+  CircleIconButton({this.size = 30.0, this.icon = Icons.clear, this.onPressed});
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: this.onPressed,
+        child: SizedBox(
+            width: size,
+            height: size,
+            child: Stack(
+              alignment: Alignment(0.0, 0.0), // all centered
+              children: <Widget>[
+                Container(
+                  width: size,
+                  height: size,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.deepOrange),
+                ),
+                Icon(
+                  icon,
+                  size: size * 0.6, // 60% width for icon
+                )
+              ],
+            )));
   }
 }
